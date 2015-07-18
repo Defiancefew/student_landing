69924a496f762c188286d17c64d771bdfb7c8f30
Defiancefew <Severancepwns@gmail.com>
2015-07-19 01:55:04 +0300

    Project commit
    Added few sections + project enhancements

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
 
+// Jquery Tabs
+$(".top_phone .tab_item").not(":first").hide();
+$(".top_phone .wrapper .tab").click(function() {
+	$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+$(".tabs_header .tab_item").not(":first").hide();
+$(".tabs_header .wrapper .tab").click(function() {
+	$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+$(".bottom_phone .tab_item").not(":first").hide();
+$(".bottom_phone .wrapper .tab").click(function() {
+	$(".bottom_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".bottom_phone .tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+$('.review').stellar();
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({items:1,dots:true});
+});
+
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
 		ga("send", "event", "goal", "goal");
